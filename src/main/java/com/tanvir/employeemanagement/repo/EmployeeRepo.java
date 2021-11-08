36ccd8b1099d052ff9e647b9e52848287519d513
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-11-09 01:59:57 +0600

    Service class created

--- src/main/java/com/tanvir/employeemanagement/repo/EmployeeRepo.java
+++ src/main/java/com/tanvir/employeemanagement/repo/EmployeeRepo.java
@@ -3,6 +3,11 @@ package com.tanvir.employeemanagement.repo;
 import com.tanvir.employeemanagement.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
+    void deleteEmploueeById(Long id);
+
+    Optional findEmployeeById(Long id);
 }
--- src/main/java/com/tanvir/employeemanagement/service/EmployeeService.java
+++ src/main/java/com/tanvir/employeemanagement/service/EmployeeService.java
@@ -4,6 +4,7 @@ import com.tanvir.employeemanagement.model.Employee;
 import com.tanvir.employeemanagement.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.UUID;
 
 public class EmployeeService {
@@ -17,5 +18,21 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException ("User by Id" + id + "was not found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmploueeById(id);
     }
 }

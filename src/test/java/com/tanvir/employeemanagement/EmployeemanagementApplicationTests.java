6b346ce2e0e3684bfe07f28913847580cda05403
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2021-11-08 22:39:02 +0600

    First Commit

---
+++ src/test/java/com/tanvir/employeemanagement/EmployeemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanvir.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
